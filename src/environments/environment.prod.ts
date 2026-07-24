593eb367e18a6bba30873a220a7c2dc1c3b2bc56
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-07-24 10:36:06 +0630

    Create environment.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  githubUsername: 'Dev-moekyawaung',
+  githubApiUrl: 'https://api.github.com',
+  siteUrl: 'https://dev-moekyawaung.github.io'
+};
